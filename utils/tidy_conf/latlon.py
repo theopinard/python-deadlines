0bfbebb391ba9f6c0d6bedab70e71e2f3c8bfabf
Jesper Dramsch <jesper@dramsch.net>
2024-03-25 02:48:16 +0100

    Extra_places

--- utils/tidy_conf/latlon.py
+++ utils/tidy_conf/latlon.py
@@ -21,8 +21,8 @@ def add_latlon(data):
             continue
         elif "location" in q:
             # If location is already present, add it to the cache
-            cache[q["place"]] = q["location"]
-            continue
+            cache[q["place"]] = q["location"][0]
+            # continue
         else:
             # Add to the copy if location is not present for speed
             data_copy.append((i, q))
@@ -36,30 +36,44 @@ def add_latlon(data):
             tqdm.write(f"IndexError: {q['place']}")
 
         # Check if the location is already in the cache
-        if q["place"] in cache and cache[q["place"]] is not None:
-            data[i]["location"] = {
-                "latitude": cache[q["place"]]["latitude"],
-                "longitude": cache[q["place"]]["longitude"],
-            }
+        places = [q["place"]]
+        if "extra_places" in q:
+            places += q["extra_places"]
 
-        else:
-            # Get the location from Openstreetmaps
-            url = "https://nominatim.openstreetmap.org/search" + "?format=json&q=" + urllib.parse.quote(q["place"])
-            response = requests.get(url)
-
-            if response:
-                try:
-                    response = response.json()
-                    data[i]["location"] = {
-                        "latitude": float(response[0]["lat"]),
-                        "longitude": float(response[0]["lon"]),
+        new_location = []
+        for place in places:
+            place = place.strip()
+            if place in cache and cache[place] is not None:
+                new_location += [
+                    {
+                        "latitude": cache[place]["latitude"],
+                        "longitude": cache[place]["longitude"],
                     }
-                    cache[q["place"]] = data[i]["location"]
-                except IndexError:
-                    cache[q["place"]] = None
-                    tqdm.write(f"No response from Openstreetmaps for {q['place']}")
-                time.sleep(2)
+                ]
+
             else:
-                cache[q["place"]] = None
-                tqdm.write(f"No response from Openstreetmaps for {q['place']}")
+                # Get the location from Openstreetmaps
+                url = "https://nominatim.openstreetmap.org/search" + "?format=json&q=" + urllib.parse.quote(place)
+                response = requests.get(url)
+
+                if response:
+                    try:
+                        response = response.json()
+                        new_location += [
+                            {
+                                "latitude": float(response[0]["lat"]),
+                                "longitude": float(response[0]["lon"]),
+                            }
+                        ]
+                        cache[place] = new_location[-1]
+                    except IndexError:
+                        cache[place] = None
+                        tqdm.write(f"No response from Openstreetmaps for {q['place']}")
+                    time.sleep(2)
+                else:
+                    cache[place] = None
+                    tqdm.write(f"No response from Openstreetmaps for {q['place']}")
+        else:
+            if new_location:
+                data[i]["location"] = new_location
     return data
